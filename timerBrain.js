1c675899e82c1c7ffc6ca1bdef8dddcd3b673b76
alexenger <alexenger@msn.com>
2020-11-27 14:45:17 -0800

    added logo, timer & list functions, basic css, selected css - with slight bug

---
+++ timerBrain.js
@@ -0,0 +1,61 @@
+function countdownTimer(){
+    //following section of JS code for 30min timer borrowed from https://codepen.io/falak786/pen/oddYYd
+    var sec         = 10,
+    countDiv    = document.getElementById("timer"),
+    secpass,
+    countDown   = setInterval(function () {
+        'use strict';      
+        secpass();
+    }, 1000);
+
+    function secpass() {
+        'use strict';    
+        var min     = Math.floor(sec / 60),
+            remSec  = sec % 60;
+    
+        if (remSec < 10) {        
+            remSec = '0' + remSec;    
+        }
+
+        if (min < 10) {        
+            min = '0' + min;    
+        }
+        countDiv.innerHTML = min + ":" + remSec;
+    
+        if (sec > 0) {   
+            sec = sec - 1;
+        } else {
+            clearInterval(countDown);
+            countDiv.innerHTML = 'Activity Time!!';
+            //End Borrowed code
+            let range=document.getElementById('activities').lastElementChild.id
+            let randact = Math.floor(Math.random() * range + 1)
+            let selected = document.getElementById(randact)
+            selected.setAttribute('id','selected')
+        }
+    }
+
+}
+
+const addToList = ()=>{
+    let entry = document.getElementById("newActivity").value
+    let prev = document.getElementById('activities').lastElementChild.id
+    let newID = ++prev
+    let activity = document.createElement('li')
+    activity.setAttribute('id', newID)
+    let brk = document.createElement('br')
+    activity.innerHTML = entry
+    document.getElementById('activities').appendChild(activity)
+    document.getElementById("newActivity").value = "";
+    event.preventDefault()
+};
+
+function reset(){
+    let acts = document.querySelectorAll('li[id]')
+    for (i=0; i<acts.length; i++){
+        if (id = 'selected'){
+            let red = document.getElementById('selected')
+            red.removeAttribute('id', 'selected')
+        }
+    }  
+}
